ed19fafd72474579727422af194405af9bc4fe82
Marcin Dubel <marcin@appsilon.com>
2023-03-07 15:27:32 +0100

    Add renv dependencies

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,8 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
+library(shiny)
+library(AmesHousing)
+library(dplyr)
+library(rlang)
+library(ggplot2)
+library(scales)
